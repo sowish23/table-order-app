2741fe078a7e7e34455583eb16e24219b79da114
sowish23 <soso2266@naver.com>
2022-06-18 18:38:36 +0900

    add routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from "./pages/Main"
+import Order from "./pages/Order"
+import Kitchen from "./pages/Kitchen"
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Main/>} />
+      <Route path="/order" element={<Order/>} />
+      <Route path="/kitchen" element={<Kitchen/>} />
+    </Routes>
   );
 }
 
